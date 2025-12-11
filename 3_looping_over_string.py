08e533d16468fd39662a188965815d744edff58c
vrodriguez7016 <vrodriguez7016@gmail.com>
2025-12-11 08:31:15 -0600

    Update 3_looping_over_string.py

--- 3_looping_over_string.py
+++ 3_looping_over_string.py
@@ -2,5 +2,19 @@
 # Ask the user for a word.
 # Use a for loop to print each letter on a new line.
 
+word = input("Type in your word: ")
+for letter in word:
+    print(letter)
+
 # Challenge:
 # Count how many vowels are in the word.
+
+vowels = "aeiouAEIOU"
+count = 0
+word1 = input("Type in your word: ")
+for letter in range(len(word1)):
+    if letter in vowels:
+        count+=1
+    else:
+        continue
+print(count)
